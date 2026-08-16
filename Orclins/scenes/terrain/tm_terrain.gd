b0ec7a0159087b9e880d6c45b77d54032e296498
George <info@spacejuice.co.uk>
2026-08-16 23:58:04 +0100

    basic random ore

--- Orclins/scenes/terrain/tm_terrain.gd
+++ Orclins/scenes/terrain/tm_terrain.gd
@@ -29,13 +29,32 @@ func _ready() -> void:
 		map_coords= Vector2i(map_x,map_y)
 	map_x = -200
 	map_y = 4
-	
-	atlas_coords= Vector2i(2,0)
+	var rng_num: int = randi_range(0,9)
+	atlas_coords= Vector2i(2,0) #stone block
 	while map_y < 200:
 		while map_x <200:
-			tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
-			map_x = map_x +1
-			map_coords= Vector2i(map_x,map_y)
+			rng_num = randi_range(-100,99)
+			if rng_num >= 40 and rng_num <= 60 and map_y > 10:
+				atlas_coords = Vector2i(3,0) #coal
+				tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
+				map_x = map_x +1
+				map_coords= Vector2i(map_x,map_y)
+			elif rng_num >= 61 and rng_num <= 75 and map_y > 20:
+				atlas_coords = Vector2i(2,1) #iron
+				tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
+				map_x = map_x +1
+				map_coords= Vector2i(map_x,map_y)
+			elif rng_num >=76  and map_y > 30:
+				print(map_y)
+				atlas_coords = Vector2i(3,1) #copper
+				tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
+				map_x = map_x +1
+				map_coords= Vector2i(map_x,map_y)
+			else:
+				atlas_coords = Vector2i(2,0)
+				tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
+				map_x = map_x +1
+				map_coords= Vector2i(map_x,map_y)
 		map_x = -200
 		tm_terrain.set_cell(map_coords,atlas_id,atlas_coords)
 		map_y = map_y + 1
